3feb44b50ffb795571deb7b9e92eb87af740f8d1
JBailes <jbailes@gmail.com>
2026-04-27 09:53:41 +0000

    Add LUN pin clear helper

--- controlplane/lun_pin.go
+++ controlplane/lun_pin.go
@@ -1,6 +1,7 @@
 package controlplane
 
 import (
+	"errors"
 	"fmt"
 
 	"golang.org/x/sys/unix"
@@ -8,9 +9,27 @@ import (
 
 const lunPinXattr = "trusted.smoothfs.lun"
 
+var (
+	setXattr    = unix.Setxattr
+	removeXattr = unix.Removexattr
+)
+
 func setLUNPin(path string) error {
-	if err := unix.Setxattr(path, lunPinXattr, []byte{1}, 0); err != nil {
+	if err := setXattr(path, lunPinXattr, []byte{1}, 0); err != nil {
 		return fmt.Errorf("set %s on %s: %w", lunPinXattr, path, err)
+	}
+	return nil
+}
+
+// ClearLUNPin clears the smoothfs LUN pin xattr after the target path has been
+// administratively quiesced. A missing xattr is already unpinned and is treated
+// as success so retrying quiesce cleanup is idempotent.
+func ClearLUNPin(path string) error {
+	if err := removeXattr(path, lunPinXattr); err != nil {
+		if errors.Is(err, unix.ENODATA) {
+			return nil
+		}
+		return fmt.Errorf("remove %s on %s: %w", lunPinXattr, path, err)
 	}
 	return nil
 }
